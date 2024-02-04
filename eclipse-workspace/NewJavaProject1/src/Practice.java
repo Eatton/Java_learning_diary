df3af7f1c4e981443813d089c920c041a1cc993f
Eatton <157953549+Eatton@users.noreply.github.com>
2024-02-04 10:00:16 +0800

    Add files via upload

---
+++ eclipse-workspace/NewJavaProject1/src/Practice.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Practice
+{
+	public static void main(String[] args)		
+	{
+		Random generator = new Random();
+		int num1;
+		double num2;
+		num1 = generator.nextInt();
+		System.out.println("A random number is: "+num1);
+		num1 = generator.nextInt(9)+1;
+		System.out.println("From 0 to 9: "+num1);
+		num2 = generator.nextFloat()*2.4+1.1;
+		System.out.println("A random float(between 0 and 1)"+ num2);
+		num1 = (int)num2 + 1;
+		System.out.println(num1);
+		
+	}
+}
+
+
